24aa55abdf8a6f36f65957aaf96e18b97425e878
Felix Gordillo <felix.gordillo@ucsp.edu.pe>
2022-12-12 12:28:29 -0500

    add feature export pdf to email

--- Form to CV/Form to CV/MyCV.xaml.cs
+++ Form to CV/Form to CV/MyCV.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using PdfSharp.Xamarin.Forms;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.Xaml;
 
 namespace Form_to_CV
@@ -24,5 +28,34 @@ namespace Form_to_CV
             education.Text = Edu;
 
         }
+
+        private async void Button_Clicked_GeneratePDF(object sender, EventArgs e)
+        {
+            var pdf = PDFManager.GeneratePDFFromView(CV_Content);
+            string filename = "mypdf.pdf";
+            string path = System.IO.Path.Combine(FileSystem.CacheDirectory, filename);
+            pdf.Save(path);
+
+            //var mensaje = new EmailMessage("My CV", "", "gordilloyucra@gmail.com");
+            //await Email.ComposeAsync(mensaje);
+            //pdf.Save(FileSystem.CacheDirectory);
+            //MemoryStream pdfStream = new MemoryStream();
+            //pdf.Save(pdfStream);
+            //File.WriteAllText(file, "Hello World");
+
+      
+            var message = new EmailMessage
+            {
+                Subject = "Hello",
+                Body = "World",
+            };
+
+            message.Attachments.Add(new EmailAttachment(path));
+
+            await Email.ComposeAsync(message);
+            
+
+        }
+
     }
 }
