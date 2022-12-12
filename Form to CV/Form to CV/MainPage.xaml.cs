61acc882ac2d8c94aafe4cfb0915432e493c0703
Felix Gordillo <felix.gordillo@ucsp.edu.pe>
2022-12-12 07:09:56 -0500

    Agregar archivos de proyecto.

---
+++ Form to CV/Form to CV/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Form_to_CV
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string name = EntryName.Text;
+            string age = EntryAge.Text;
+            string contact = EntryContact.Text;
+            string occupation = EntryOccupation.Text;
+            string skill = EntrySkill.Text;
+            string education = EntryEducation.Text;
+            Application.Current.MainPage.Navigation.PushModalAsync(new MyCV(name, age, contact, occupation, skill, education), true);
+        }
+
+        private void Button_Clicked_Ocupation(object sender, EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter your occupation" };
+            ocupation.Children.Add(entry);
+
+        }
+        private void Button_Clicked_Skill(object sender, EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter your skill" };
+            skill.Children.Add(entry);
+        }
+        private void Button_Clicked_Education(object sender, EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter your Education" };
+            education.Children.Add(entry);
+        }
+    }
+}
---
+++ Form to CV/Form to CV/MyCV.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Form_to_CV
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyCV : ContentPage
+    {
+        public MyCV(string Name, string Age, string Contact, string Occupation, string Skill, string Edu)
+        {
+            InitializeComponent();
+
+            name.Text = Name;
+            age.Text = Age;
+            contact.Text = Contact;
+            occupation.Text = Occupation;
+            skill.Text = Skill;
+            education.Text = Edu;
+
+        }
+    }
+}
